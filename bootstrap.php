54bb614cc7e4b4b7469b2b90e84341d418a62a86
Andrew Tereshko <andrew.tereshko@gmail.com>
2013-01-03 13:21:46 +0000

    Add mbstring default encoding (utf-8)

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,9 @@
  */
 date_default_timezone_set('Europe/Moscow');
 
+mb_internal_encoding('UTF-8');
+mb_language('uni');
+
 error_reporting( E_ALL );
 
 include_once __DIR__ . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'Erum.php';
