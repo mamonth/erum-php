ef530d34133beb89846962b2f9c88c18d99008bf
Andrew Tereshko <andrew.tereshko@gmail.com>
2013-05-18 10:27:17 +0000

    make some beauty in default app config

--- configs/application.default.php
+++ configs/application.default.php
@@ -1,7 +1,8 @@
 <?php
 
 return array(
-    'modulesRoot' => dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR . 'modules',
-    'libsRoot' => dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR . 'libs',
-    'debug' => false,
+    'modulesRoot'   => dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR . 'modules',
+    'libsRoot'      => dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR . 'libs',
+    'configsRoot'   => dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR . 'configs',
+    'debug'         => false,
 );
